f5790dc3c1f10c2a5033d0a3585b816d80033cba
Jacob <92279567+Oia20@users.noreply.github.com>
2024-07-06 18:17:38 -0500

    Added landing component where I will have the homepage to formbee.

---
+++ client/FormBee/src/app/landing/landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingComponent {
+
+}
