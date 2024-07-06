a7b9e294c342845a88b1040e78527d74d79df554
Jacob <92279567+Oia20@users.noreply.github.com>
2024-07-06 17:41:30 -0500

    Form submisson no longer works, unless verified.

--- client/FormBee/src/app/contact/contact.component.ts
+++ client/FormBee/src/app/contact/contact.component.ts
@@ -1,23 +1,29 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import ngForm from '@angular/forms';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { NgIf, CommonModule } from '@angular/common'; // Imported CommonModule for NgIf
 import { HttpClient } from '@angular/common/http';
-import  'altcha';
+import 'altcha';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ NgIf, ReactiveFormsModule],
+  imports: [CommonModule, NgIf],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss',
+  styleUrls: ['./contact.component.scss'], // Corrected from styleUrl to styleUrls
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit, AfterViewInit {
   contactForm: FormGroup;
+  altchaElement: HTMLElement | null = null;
+  isVerified = false; // Flag to track verification state
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(
+    private fb: FormBuilder, 
+    private http: HttpClient, 
+    private elRef: ElementRef<HTMLElement>,
+    private renderer: Renderer2,
+    
+    ) {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -25,14 +31,34 @@ export class ContactComponent {
     });
   }
   
+  ngOnInit() {
+    // Component setup can be done here
+  }
+
+  ngAfterViewInit() {
+    // Set up the event listener after the view has initialized
+    this.altchaElement = this.elRef.nativeElement.querySelector('#altcha');
+    if (this.altchaElement) {
+      this.renderer.listen(this.altchaElement, 'statechange', (ev) => {
+        console.log('state:', ev.detail.state);
+        if (ev.detail.state === 'verified') {
+          this.isVerified = true;
+          console.log('payload:', ev.detail.payload);
+        } else {
+          this.isVerified = false;
+        }
+      });
+    }
+    console.log("altcha element: ", this.altchaElement);
+  }
+
   onSubmit() {
-    console.log(this.contactForm.value);
-    
-    this.http.post('http://localhost:3000/', this.contactForm.value).subscribe(res => {
-
-      console.log(res);
-
-    });
-
+    if (this.isVerified) {
+      this.http.post('http://localhost:3000/', this.contactForm.value).subscribe(res => {
+        console.log(res);
+      });
+    } else {
+      console.error('Cannot submit: Not verified.');
+    }
   }
 }
