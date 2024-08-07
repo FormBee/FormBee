9a72a8c365955a2d9735bf72b16d0fb8f17d5815
Jacob <92279567+Oia20@users.noreply.github.com>
2024-08-07 14:59:42 -0400

    Return email modal styling

--- client/FormBee/src/app/dashboard-return-modal/dashboard-return-modal.component.ts
+++ client/FormBee/src/app/dashboard-return-modal/dashboard-return-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, output } from '@angular/core';
 import { ElementRef } from '@angular/core';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard-return-modal',
   standalone: true,
@@ -7,8 +8,13 @@ import { ElementRef } from '@angular/core';
   templateUrl: './dashboard-return-modal.component.html',
   styleUrl: './dashboard-return-modal.component.scss'
 })
-export class DashboardReturnModalComponent {
+export class DashboardReturnModalComponent implements OnInit {
   elRef: ElementRef<HTMLElement>;
+  switch: boolean = false;
+
+  ngOnInit(): void {
+    this.setSwitch();
+  }
 
   constructor(elRef: ElementRef<HTMLElement>) {
     this.elRef = elRef;
@@ -29,5 +35,16 @@ export class DashboardReturnModalComponent {
         this.returnEmailModal.emit(false);
       }, 500); // Wait for the animation to finish
     }
+  }
+  toggleSwitch() {
+    const modalSwitch = document.getElementById('toggleSwitch') as HTMLInputElement;
+    this.switch = modalSwitch.checked;
+    console.log(this.switch);
+  }
+
+  setSwitch() {
+    const modalSwitch = document.getElementById('toggleSwitch') as HTMLInputElement;
+    modalSwitch.checked = this.switch;
+    console.log(this.switch);
   }
 }
