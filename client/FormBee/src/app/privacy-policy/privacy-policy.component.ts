9625b5b00434651cd824631ff458757d4f1c1cf9
Jacob <92279567+Oia20@users.noreply.github.com>
2024-08-20 11:13:16 -0500

    Footer added to privacy policy, and info is now our real privacy policy.

--- client/FormBee/src/app/privacy-policy/privacy-policy.component.ts
+++ client/FormBee/src/app/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { LandingNavComponent } from '../landing-nav/landing-nav.component';
 import { LandingThreeBgComponent } from '../landing-three-bg/landing-three-bg.component';
+import { LandingFooterComponent } from '../landing-footer/landing-footer.component';
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [LandingNavComponent,
+  imports: [
+    LandingNavComponent,
     LandingThreeBgComponent,
+    LandingFooterComponent,
   ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
--- client/FormBee/src/app/terms-of-service/terms-of-service.component.ts
+++ client/FormBee/src/app/terms-of-service/terms-of-service.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { LandingThreeBgComponent } from '../landing-three-bg/landing-three-bg.component';
 import { LandingNavComponent } from '../landing-nav/landing-nav.component';
+import { LandingFooterComponent } from '../landing-footer/landing-footer.component';
 @Component({
   selector: 'app-terms-of-service',
   standalone: true,
   imports: [
     LandingNavComponent,
     LandingThreeBgComponent,
+    LandingFooterComponent,
   ],
   templateUrl: './terms-of-service.component.html',
   styleUrl: './terms-of-service.component.scss'
