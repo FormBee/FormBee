29f35f0ccd31285d3805984b42fe347a035da9b1
Jacob <92279567+Oia20@users.noreply.github.com>
2024-09-13 20:53:09 -0500

    Datasource changed to dev.

--- server/MyProject/src/data-source.ts
+++ server/MyProject/src/data-source.ts
@@ -4,30 +4,30 @@ import { User } from "./entity/User"
 const dotenv = require('dotenv');
 dotenv.config();
 
-// export const AppDataSource = new DataSource({
-    // type: "postgres",
-    // host: process.env.DEV_DB_HOST,
-    // port: 1234, // Change this to match your database's port
-    // username: process.env.DEV_DB_USER,
-    // password: process.env.DEV_DB_PASS,
-    // database: process.env.DEV_DB,
-    // synchronize: true,
-    // logging: false,
-    // entities: [User],
-    // migrations: [],
-    // subscribers: [],
-// })
-
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.PROD_DB_HOST,
-    port: 39159,
-    username: process.env.PROD_DB_USER,
-    password: process.env.PROD_DB_PASS,
-    database: process.env.PROD_DB,
+    host: process.env.DEV_DB_HOST,
+    port: 5433, // Change this to match your database's port
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASS,
+    database: process.env.DEV_DB,
     synchronize: true,
     logging: false,
     entities: [User],
     migrations: [],
     subscribers: [],
 })
+
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     host: process.env.PROD_DB_HOST,
+//     port: 39159,
+//     username: process.env.PROD_DB_USER,
+//     password: process.env.PROD_DB_PASS,
+//     database: process.env.PROD_DB,
+//     synchronize: true,
+//     logging: false,
+//     entities: [User],
+//     migrations: [],
+//     subscribers: [],
+// })
