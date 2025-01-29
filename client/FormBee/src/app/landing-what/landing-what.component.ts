afd53c971dd6b36719eb24baae7d33ebc1a45e6a
Jacob <92279567+Oia20@users.noreply.github.com>
2025-01-28 23:17:33 -0600

    Replaced landing page img URL's with uploadthing hosted URLs.

--- client/FormBee/src/app/landing-what/landing-what.component.ts
+++ client/FormBee/src/app/landing-what/landing-what.component.ts
@@ -11,13 +11,13 @@ import { NgFor } from '@angular/common';
 })
 export class LandingWhatComponent implements OnInit {
   plugins = [
-    { icon: 'https://gliscfokeivkvdrwzlsv.supabase.co/storage/v1/object/public/Plugin%20images/previous-return.svg', name: 'Return logo', description: 'Send an automatic reply email from your email address.' },
-    { icon: 'https://gliscfokeivkvdrwzlsv.supabase.co/storage/v1/object/public/Plugin%20images/n8n%20logo.png', name: 'n8n logo', description: 'Create automations on n8n.io with your form data.' },
+    { icon: 'https://pgtgy4em2f.ufs.sh/f/oMW3imFO9N6PhUrhRH9oNEvT3zUg8ShqaJ7yIfuC2rmDVjnp', name: 'Return logo', description: 'Send an automatic reply email from your email address.' },
+    { icon: 'https://pgtgy4em2f.ufs.sh/f/oMW3imFO9N6Pa1cDi3IdtKLjOUncgNeulwxE40SBZHF6XdQC', name: 'n8n logo', description: 'Create automations on n8n.io with your form data.' },
     // { icon: 'https://gliscfokeivkvdrwzlsv.supabase.co/storage/v1/object/public/Plugin%20images/slack%20logo.png', name: 'Slack logo', description: 'Receive your form submissions in your Slack channel.' },
-    { icon: 'https://gliscfokeivkvdrwzlsv.supabase.co/storage/v1/object/public/Plugin%20images/discord%20logo.webp', name: 'Discord logo', description: 'Receive your form submissions in your discord channel.' },
-    { icon: 'https://gliscfokeivkvdrwzlsv.supabase.co/storage/v1/object/public/Plugin%20images/make%20logo.png', name: 'Make logo', description: 'Create automations on make.com with your form data.' },
-    { icon: 'https://gliscfokeivkvdrwzlsv.supabase.co/storage/v1/object/public/Plugin%20images/Telegram-Logo.png', name: 'Telegram logo', description: 'Receive your form submissions via Telegram.' },
-    { icon: 'https://gliscfokeivkvdrwzlsv.supabase.co/storage/v1/object/public/Plugin%20images/wehhook%20logo.png', name: 'Webhook logo', description: 'Receive your form submissions with custom webhooks.' },
+    { icon: 'https://pgtgy4em2f.ufs.sh/f/oMW3imFO9N6PSmiJaLNB0zJC6DQF28kIwYEHsdoeNgPUTlWS', name: 'Discord logo', description: 'Receive your form submissions in your discord channel.' },
+    { icon: 'https://pgtgy4em2f.ufs.sh/f/oMW3imFO9N6PgfmW5Q6uhT2XaGNP3VOC10UYjb6AWywDpiBE', name: 'Make logo', description: 'Create automations on make.com with your form data.' },
+    { icon: 'https://pgtgy4em2f.ufs.sh/f/oMW3imFO9N6PcgEnoCvbMOvsrnRhEwqLJa7FIe6Gtj0Cg5Wl', name: 'Telegram logo', description: 'Receive your form submissions via Telegram.' },
+    { icon: 'https://pgtgy4em2f.ufs.sh/f/oMW3imFO9N6PzukFbxWEBo2Su7PjCQAxm0LK6gfU51spibdD', name: 'Webhook logo', description: 'Receive your form submissions with custom webhooks.' },
 
   ];
 
